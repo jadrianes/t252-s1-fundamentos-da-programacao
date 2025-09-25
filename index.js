f752dcad2b42a2bfa700004d51a326bef0f13062
jessicasouza <jeadrianesouza@gmail.com>
2025-09-25 16:02:36 -0300

    Inicio do desafio

--- index.js
+++ index.js
@@ -5,5 +5,24 @@
 //     maiorDeIdade: false
 // }
 
-const palavra = 24
-console.log(palavra.toUpperCase())
+// const palavra = 24
+// console.log(palavra.toUpperCase())
+
+// const somar = function (a: number, b: number): number
+// {
+// return a + b;
+// };
+// console.log(somar ("2", "3"));
+
+
+// function verificarNota (nota: number): string {
+// if (nota >= 7) {
+// let resultado = "Aprovada";
+// return resultado;
+// } else {
+// let resultado = "Reprovada";
+// return resultado;
+// }
+// console.log(resultado);
+// }
+// console.log(verificarNota(8));
